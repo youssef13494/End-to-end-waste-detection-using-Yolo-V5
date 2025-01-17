6c919e25ee13c904325f5477de4d96e24c44c5ea
Youssef Hatem <youssefhatem206@gmail.com>
2025-01-17 11:31:33 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wasteDetection',
+    version='0.0.0',
+    auther='Youssef Hatem',
+    author_email='youssefhatem206@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
