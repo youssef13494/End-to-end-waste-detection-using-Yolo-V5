b4a67d96cb6c9c1c9a0a243fe7dae6d421cd6647
Youssef Hatem <youssefhatem206@gmail.com>
2025-01-20 19:47:31 +0200

    data ingestion added

--- app.py
+++ app.py
@@ -1,41 +1,4 @@
-import os.path
-import yaml
-import base64
-from wasteDetection.logger import logging
-from wasteDetection.exception import AppException
-import sys
+from wasteDetection.pipeline.training_pipeline import TrainPipeline
 
-def read_yaml_file(file_path: str)->dict:
-    try:
-        with open(file_path,'rb') as yaml_file:
-            logging.info(f'Reading yaml file successfully')
-            return yaml.safe_load(yaml_file)
-    except Exception as e:
-        raise AppException(e,sys) from e
-    
-
-
-def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
-    try:
-        if replace:
-            if os.path.exists(file_path):
-                os.remove(file_path)
-        
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        
-        with open(file_path, "w") as file:
-            yaml.dump(content, file)
-            logging.info("Successfully wrote YAML file")
-    
-    except Exception as e:
-        raise AppException(e, sys) from e
-
-
-def decodeImage(imgstring, fileName):
-    imgdata = base64.b64decode(imgstring)
-    with open("./data/" + fileName, 'wb') as f:
-        f.write(imgdata)
-        f.close()
-def encodeImageIntoBase64(croppedImagePath):
-    with open(croppedImagePath, "rb") as f:
-        return base64.b64encode(f.read())
+obj=TrainPipeline()
+obj.run_pipeline()
---
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -0,0 +1,8 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR : str = "features_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1Y7rA7JspKccycs5cv1avoSes4xCFuoWz/view?usp=drive_link"
